461abfe3a68f5cab6010cf715994a6b19d2544c8
hafezabadian <hafezabadian@gmail.com>
2023-02-06 02:07:04 +0330

    Db & Entity

---
+++ EasyPay.Data/DatabaseContext/EasyPayDbcontext.cs
@@ -0,0 +1,23 @@
+﻿using EasyPay.Data.Model;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyPay.Data.DatabaseContext
+{
+    internal class EasyPayDbcontext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionBuilder)
+        {
+            optionBuilder.UseSqlServer(@"Data Source=DESKTOP-NDNJE1O\HAFEZDB;Initial Catalog=EasyPay;Integrated Security=True;MultipleActiveResultSets=True;TrustServerCertificate=True") ;
+        }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Photo> Photos { get; set; }
+        public DbSet<BankCard> BankCards { get; set; }
+    }
+    
+}
+
---
+++ EasyPay.Data/Model/BankCard.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EasyPay.Data.Model
+{
+    public class BankCard : BaseEntity<string>
+    {
+        public BankCard()
+        {
+            Id=Guid.NewGuid().ToString();
+            DateCreated=DateTime.Now;
+            DateModified=DateTime.Now;
+        }
+        [Required]
+        public string BankName { get; set; }
+        public string Shaba { get; set; }
+        [Required]
+        [Range(16, 16)]
+        public string CardNumber { get; set; }
+        [Required]
+        [StringLength(2, MinimumLength = 2)]
+        public string ExpireDateMonth { get; set; }
+        [Required]
+        [StringLength(2, MinimumLength = 2)]
+        public string ExpireDateYear { get; set; }
+
+        [Required]
+        public string UserId { get; set; }
+        public User User { get; set; }
+    }
+}
